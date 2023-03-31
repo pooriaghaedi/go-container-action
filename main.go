89ca29ae0422a00611b8cfddda53d8d9a2d4985c
pooriaghaedi <pooria.ghaedi@gmail.com>
2023-03-31 17:29:26 +0200

    init commit

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
 
 	cmd := exec.Command("/app/k8sgpt" , "analyze" , "--explain",  "--namespace=default" , "--filter=Pod", "--output=json")
 	output, _ := cmd.CombinedOutput()
-	fmt.Println(fmt.Sprintf("Result=%s", string(output)))
+	fmt.Println(fmt.Sprintf("myOutput=%s", string(output)))
     if err := cmd.Run(); err != nil{
        fmt.Println(err)
     }
