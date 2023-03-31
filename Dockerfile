5676b06a74267a90ccc6346d86f1880640feb72a
pooriaghaedi <pooria.ghaedi@gmail.com>
2023-03-31 13:25:03 +0200

    init commit

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,16 @@
-FROM golang:1.13 as builder
+FROM golang:1.21 as builder
 
 WORKDIR /app
-COPY . /app
+COPY main.go /app
 
-RUN go get -d -v
+RUN go run main.go
+RUN wget -qO- https://github.com/k8sgpt-ai/k8sgpt/releases/download/v0.1.4/k8sgpt_Linux_x86_64.tar.gz  | tar xvzf -
 
-# Statically compile our app for use in a distroless container
-RUN CGO_ENABLED=0 go build -ldflags="-w -s" -v -o app .
 
-# A distroless container image with some basics like SSL certificates
-# https://github.com/GoogleContainerTools/distroless
 FROM gcr.io/distroless/static
 
-COPY --from=builder /app/app /app
+COPY --from=builder /app/k8sgpt* /app
 
-ENTRYPOINT ["/app"]
+RUN mkdir /root/.kube/ && cp k8sgptconfig /root/.kube/config && cp k8sgpt.yaml /root/.k8sgpt.yaml
+
+ENTRYPOINT ["./k8sgpt", "analyze", "--explain ", "--namespace=default", " --filter=Pod"]
--- main.go
+++ main.go
@@ -2,13 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
+type myData struct {
+        Backend_type          string  `yaml:"backend_type"`
+        Kubernetesclient      string
+        Openai_key 			  string  `yaml:"openai_key"`
+}
+
+func marshalYaml(filename string) ([]byte, error) {
+	
+	out, err := yaml.Marshal(&myData{Backend_type: os.Getenv("BACKENDTYPE"),Kubernetesclient: "{}",Openai_key: os.Getenv("OAIKEY")})
+    if err != nil {
+        return nil, fmt.Errorf("in file %q: %w", filename, err)
+    }
+	fmt.Println(string(out[:]))
+    return out, err
+}
+
 func main() {
-	myInput := os.Getenv("INPUT_MYINPUT")
 
-	output := fmt.Sprintf("Hello %s", myInput)
+	filename := "k8sgpt.yaml"
+	_,err := os.Create(filename)
+	if err != nil {
+    	 fmt.Println( err)
+    }
+	out, err := marshalYaml(filename)
+	if err != nil {
+        log.Fatal(err)
+    }
+	err = os.WriteFile(filename, out, 0644)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	fmt.Println(fmt.Sprintf(`::set-output name=myOutput::%s`, output))
+	k8sfilename := "k8sgptconfig"
+	err = os.WriteFile(k8sfilename, []byte(os.Getenv("K8SCONFIG")), 0644)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
